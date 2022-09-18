942b2dd51d4852fcf2b792e46bc290d876cdaa73
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-09-18 23:59:30 +0600

    Create IG.py

---
+++ IG.py
@@ -0,0 +1,31 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from i import mahadi
+ 
+        mahadi()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from IG import mahadi
+ 
+ 
+        mahadi()
+ 
